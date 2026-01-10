4fae2b585cd62903955eaba2974cae78b4a9ff29
roeiamit <roeibus13@gmail.com>
2026-01-10 13:15:57 +0200

    honeypot container abstraction

--- src/honeypot/container.py
+++ src/honeypot/container.py
@@ -1,21 +1,34 @@
-import logging
-import os
 from abc import ABC, abstractmethod
+from functools import cached_property
+from pathlib import Path
+import logging
 
 from docker import DockerClient
 from docker.errors import ImageNotFound
+from docker.models.containers import Container
+
+BASE_DIR = Path(__file__).resolve().parent
+LOCAL_RESOURCES_DIR = BASE_DIR / "resources"  # RESOURCES LOCAL PATH CONST
 
 logger = logging.getLogger(__name__)
 
-class Container(ABC):
 
-    def __init__(self, client: DockerClient) -> None:
+class HoneypotContainer(ABC):
+
+    def __init__(self, client: DockerClient,
+                 context_path: str | None = None) -> None:
         self._client = client
+        self._context_path = context_path or LOCAL_RESOURCES_DIR
+
+    @cached_property
+    @abstractmethod
+    def container(self) -> Container:
+        pass
 
     @abstractmethod
     @property
     def image_name(self) -> str:
-        ...
+        pass
 
     @abstractmethod
     def setup(self) -> None:
@@ -31,6 +44,4 @@ class Container(ABC):
             self._client.images.get(self.image_name)
         except ImageNotFound:
             logger.info(f"Image not found: {self.image_name}, building...")
-            self._client.images.build(path=os.getcwd(), tag=self.image_name)
-
-os.getcwd()
+            self._client.images.build(path=self._context_path, tag=self.image_name)
