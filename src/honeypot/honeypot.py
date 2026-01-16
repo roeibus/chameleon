9d6acef61fc21e608c67f6bdd8af7c848739df78
Roei <roeibus13@gmail.com>
2026-01-16 18:16:58 +0200

    removed useless abstraction

--- src/honeypot/honeypot.py
+++
@@ -1,9 +0,0 @@
-import asyncio
-from abc import ABC, abstractmethod
-
-
-class Honeypot(ABC):
-
-    @abstractmethod
-    async def handle_client(self, reader: asyncio.StreamReader, writer: asyncio.StreamWriter) -> None:
-        pass
