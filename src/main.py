71ccbd307aec566b886b77d574e7782633a7f2da
Roei <roeibus13@gmail.com>
2026-01-16 17:54:04 +0200

    prettified the main, currently is used only to check the code actually works

--- src/main.py
+++ src/main.py
@@ -3,24 +3,23 @@ import logging
 import docker
 
 from honeypot.connection.telnet import TelnetBridge
-from honeypot.container import HoneypotContainer, ContainerConfig
+from honeypot.container import HoneypotContainer, ContainerConfig, LOCAL_RESOURCES_DIR
 
-# Configure logging
+# TODO: configure a better logger than the basic config one
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
 
 docker_client = docker.from_env()
-TELNET_CONTAINER = HoneypotContainer(docker_client, ContainerConfig("telnet"))
-TELNET = {"port": 2323, "container": TELNET_CONTAINER }
+TELNET_CONTEXT_PATH = (LOCAL_RESOURCES_DIR / "telnet")
+TELNET_CONTAINER = HoneypotContainer(docker_client, ContainerConfig("telnet"), TELNET_CONTEXT_PATH)
+TELNET = {"port": 2323, "container": TELNET_CONTAINER}
+
 
 async def start_server():
-    async def client_connected_cb(reader, writer):
-        bridge = TelnetBridge(TELNET["container"])
-
-        await bridge.handle_client(reader, writer)
+    bridge = TelnetBridge(TELNET["container"])
 
     server = await asyncio.start_server(
-        client_connected_cb,
+        bridge.handle_client,
         host='0.0.0.0',
         port=TELNET["port"]
     )
