eae430d70472a1d8ceec0371f7143dfd9e350909
Roei <roeibus13@gmail.com>
2026-01-16 17:54:20 +0200

    implemented smarter telnet fake greeting

--- src/honeypot/connection/telnet.py
+++ src/honeypot/connection/telnet.py
@@ -1,15 +1,25 @@
-from asyncio import StreamWriter
-from honeypot.client import SessionBridge  # Assuming you saved the bridge code here
+import asyncio
+import logging
+from asyncio import StreamWriter, StreamReader
+
+from honeypot.client import ContainerSessionBridge
+
+logger = logging.getLogger(__name__)
 
 
-class TelnetBridge(SessionBridge):
+class TelnetBridge(ContainerSessionBridge):
 
-    async def greet(self, writer: StreamWriter) -> None:
-        banner = (
-            b"\r\n"
-            b"Ubuntu 18.04.6 LTS\r\n"
-            b"\r\n"
-            b"server login: "
-        )
-        writer.write(banner)
-        await writer.drain()
+    async def greet(self, reader: StreamReader,
+                    writer: StreamWriter) -> None:
+        try:
+            writer.write(b"Ubuntu 20.04 LTS\r\nlogin: ")
+            await writer.drain()
+            await reader.readline()  # Username
+            writer.write(b"Password: ")
+            await writer.drain()
+            await reader.readline()  # Password
+            await asyncio.sleep(1)
+            writer.write(b"\r\nWelcome to Ubuntu.\r\n\r\n")
+            await writer.drain()
+        except Exception as e:
+            logger.error(f"Connection error: {e}")
